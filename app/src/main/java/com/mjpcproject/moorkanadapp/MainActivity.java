bbe9406b7213022dff98e8ae6ccd00bcb97a6a20
shaniOO3 <official.solo003@gmail.com>
2020-08-22 12:27:27 +0530

    Initial commit

---
+++ app/src/main/java/com/mjpcproject/moorkanadapp/MainActivity.java
@@ -0,0 +1,52 @@
+package com.mjpcproject.moorkanadapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.ViewFlipper;
+
+public class MainActivity extends AppCompatActivity {
+
+    ViewFlipper v_flipper;
+    CardView map_view;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        int images[] = {R.drawable.news,R.drawable.coming_soon};
+
+        v_flipper = findViewById(R.id.v_flipper);
+        map_view = findViewById(R.id.map_view);
+
+        for(int image: images){
+            flipperImages(image);
+        }
+
+        map_view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(),MapPage.class));
+            }
+        });
+
+    }
+
+    public void flipperImages(int image){
+        ImageView imageView = new ImageView(this);
+        imageView.setBackgroundResource(image);
+
+        v_flipper.addView(imageView);
+        v_flipper.setFlipInterval(4000);
+        v_flipper.setAutoStart(true);
+
+        v_flipper.setInAnimation(this,android.R.anim.fade_in);
+        v_flipper.setOutAnimation(this,android.R.anim.fade_out);
+    }
+
+}
